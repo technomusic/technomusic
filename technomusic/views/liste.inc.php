b0e1dff9f3fc3287b3ea4d014cb4c4461acdc9ed
Flink88 <meanf.isat+github@gmail.com>
2015-08-10 16:10:39 +0200

    pre modif de list.inc.php

--- technomusic/views/liste.inc.php
+++ technomusic/views/liste.inc.php
@@ -5,15 +5,28 @@
 
     try
     {
-        $dbh = new PDO("mysql:host=$hostname;dbname=filhebdo",   $username, $password);
+        $dbh = new PDO("mysql:host=$hostname;dbname=$dbname",   $username, $password);
         //$dbh = new PDO("sqlite:./data/movies.db");
         $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $sql = "SELECT id, title, year, director FROM movies";
+        $sql = "SELECT Chanson_ID, Titre, Duree, Annee, Description, Categorie_ID FROM chanson";
         $stmt = $dbh->query($sql);
         while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
         {
-             echo "<tr><td><a href=\"?section=affiche-movie-exec&id=" . $row["id"] . "\">" . $row["title"] . "</a></td><td>" . $row["year"] . "</td><td>" . $row["director"] . "</td></tr>";
-        }
+            $cat = "";
+            $dbh2 = new PDO("mysql:host=$hostname;dbname=$dbname",   $username, $password);
+            //$dbh = new PDO("sqlite:./data/movies.db");
+            $dbh2->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $sql2 = "SELECT Nom FROM categorie WHERE Categorie_ID = :id";
+            $stmt2 = $dbh2->prepare($sql2);
+            $stmt2->bindValue("id", $row["Categorie_ID"]);
+            $stmt2->execute();
+            while ($row2 = $stmt2->fetch(PDO::FETCH_ASSOC))
+            {
+                $cat = $row2["Nom"];
+            }
+            unset($dbh2);
+            echo "<tr><td><a href=\"?section=affiche-chanson-exec&id=" . $row["Chanson_ID"] . "\">" . $row["Titre"] . "</a></td><td>" . $cat ."</td></tr>";
+            }
         unset($dbh);
     }
     catch(PDOException $e)
