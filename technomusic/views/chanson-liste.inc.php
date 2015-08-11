ffa59f4da49c2fcdfc6ec1a642028cfefd372ea7
Flink88 <meanf.isat+github@gmail.com>
2015-08-11 12:30:21 +0200

    modif chanson_liste.inc.php

--- technomusic/views/chanson-liste.inc.php
+++ technomusic/views/chanson-liste.inc.php
@@ -8,12 +8,27 @@
         $dbh = new PDO("mysql:host=$hostname;dbname=$dbname",   $username, $password);
         //$dbh = new PDO("sqlite:./data/movies.db");
         $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $sql = "SELECT chanson.Chanson_ID, chanson.Titre, chanson.Duree, chanson.Annee, chanson.Description, categorie.Nom, interprete_chanson.Chanson_ID, artiste.Nom FROM chanson JOIN categorie ON chanson.Categorie_ID=categorie.Categorie_ID JOIN interprete_chanson ON chanson.Chanson_ID=interprete_chanson.Chanson_ID JOIN artiste ON interprete_chanson.Atriste_ID=artiste.Atriste_ID";
+        $sql = "SELECT chanson.Chanson_ID AS id, chanson.Titre AS titre, chanson.Duree AS duree, chanson.Annee AS annee, chanson.Description AS desccription, categorie.Nom AS nomcat, artiste.Nom AS nomart FROM chanson JOIN categorie ON chanson.Categorie_ID=categorie.Categorie_ID JOIN interprete_chanson ON chanson.Chanson_ID=interprete_chanson.Chanson_ID JOIN artiste ON interprete_chanson.Artiste_ID=artiste.Artiste_ID";
         $stmt = $dbh->query($sql);
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        $id = $row["id"];
+        $titre = $row["titre"];
+        $cat = $row["nomcat"];
+        $interprete = $row["nomart"];
         while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
-        {            
-            echo "<tr><td><a href=\"?section=affiche-chanson-exec&id=" . $row["Chanson_ID"] . "\">" . $row["Titre"] . "</a></td><td>" . $cat ."</td><td>" . $interprete . "</td></tr>";
+        {    
+            if($id == $row["id"]){
+                $interprete = $interprete . "<br/>" . $row["nomart"];
+            }
+            else{
+                echo "<tr><td><a href=\"?section=affiche-chanson-exec&id=" . $id . "\">" . $titre . "</a></td><td>" . $cat ."</td><td>" . $interprete . "</td></tr>";
+                $id = $row["id"];
+                $titre = $row["titre"];
+                $cat = $row["nomcat"];
+                $interprete = $row["nomart"];
+            }   
         }
+        echo "<tr><td><a href=\"?section=affiche-chanson-exec&id=" . $id . "\">" . $titre . "</a></td><td>" . $cat ."</td><td>" . $interprete . "</td></tr>";
         unset($dbh);
     }
     catch(PDOException $e)
