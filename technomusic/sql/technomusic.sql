7590e7fb8189af55c9633567038a74554f8c9b33
Flink88 <meanf.isat+github@gmail.com>
2015-08-11 11:39:04 +0200

    coorectio artiste_ID dans toutes les tables de technomusic.sql

--- technomusic/sql/technomusic.sql
+++ technomusic/sql/technomusic.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Lun 10 Août 2015 à 16:50
+-- Généré le :  Mar 11 Août 2015 à 11:37
 -- Version du serveur :  5.6.17
 -- Version de PHP :  5.5.12
 
@@ -51,22 +51,22 @@ INSERT INTO `album` (`Album_ID`, `Titre`, `Annee`, `Label_ID`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `artiste` (
-  `Atriste_ID` bigint(20) NOT NULL AUTO_INCREMENT,
+  `Artiste_ID` bigint(20) NOT NULL AUTO_INCREMENT,
   `Nom` varchar(50) CHARACTER SET utf8 NOT NULL,
   `Prenom` varchar(50) CHARACTER SET utf8 NOT NULL,
   `Surnom` varchar(50) CHARACTER SET utf8 DEFAULT NULL,
   `Date_Naissance` date DEFAULT NULL,
   `Lieu_Naissance` varchar(50) CHARACTER SET utf8 DEFAULT NULL,
   `Bio` varchar(10000) CHARACTER SET utf8 DEFAULT NULL,
-  PRIMARY KEY (`Atriste_ID`),
-  UNIQUE KEY `ID_Artiste_IND` (`Atriste_ID`)
+  PRIMARY KEY (`Artiste_ID`),
+  UNIQUE KEY `ID_Artiste_IND` (`Artiste_ID`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
 -- Contenu de la table `artiste`
 --
 
-INSERT INTO `artiste` (`Atriste_ID`, `Nom`, `Prenom`, `Surnom`, `Date_Naissance`, `Lieu_Naissance`, `Bio`) VALUES
+INSERT INTO `artiste` (`Artiste_ID`, `Nom`, `Prenom`, `Surnom`, `Date_Naissance`, `Lieu_Naissance`, `Bio`) VALUES
 (1, 'artiste1', 'artiste1b', NULL, NULL, NULL, NULL),
 (2, 'artiste2', 'artiste2b', NULL, NULL, NULL, NULL);
 
@@ -148,17 +148,17 @@ INSERT INTO `chanson_album` (`Album_ID`, `Chanson_ID`, `Num_piste`) VALUES
 
 CREATE TABLE IF NOT EXISTS `compositeur_album` (
   `Album_ID` bigint(20) NOT NULL,
-  `Atriste_ID` bigint(20) NOT NULL,
-  PRIMARY KEY (`Album_ID`,`Atriste_ID`),
-  UNIQUE KEY `ID_ComposeAlbum_IND` (`Album_ID`,`Atriste_ID`),
-  KEY `FKCom_Art_IND` (`Atriste_ID`)
+  `Artiste_ID` bigint(20) NOT NULL,
+  PRIMARY KEY (`Album_ID`,`Artiste_ID`),
+  UNIQUE KEY `ID_ComposeAlbum_IND` (`Album_ID`,`Artiste_ID`),
+  KEY `FKCom_Art_IND` (`Artiste_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `compositeur_album`
 --
 
-INSERT INTO `compositeur_album` (`Album_ID`, `Atriste_ID`) VALUES
+INSERT INTO `compositeur_album` (`Album_ID`, `Artiste_ID`) VALUES
 (1, 1),
 (2, 2);
 
@@ -169,18 +169,18 @@ INSERT INTO `compositeur_album` (`Album_ID`, `Atriste_ID`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `compositeur_chanson` (
-  `Atriste_ID` bigint(20) NOT NULL,
+  `Artiste_ID` bigint(20) NOT NULL,
   `Chanson_ID` bigint(20) NOT NULL,
-  PRIMARY KEY (`Chanson_ID`,`Atriste_ID`),
-  UNIQUE KEY `ID_Compose_IND` (`Chanson_ID`,`Atriste_ID`),
-  KEY `FKCom_Art_1_IND` (`Atriste_ID`)
+  PRIMARY KEY (`Chanson_ID`,`Artiste_ID`),
+  UNIQUE KEY `ID_Compose_IND` (`Chanson_ID`,`Artiste_ID`),
+  KEY `FKCom_Art_1_IND` (`Artiste_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `compositeur_chanson`
 --
 
-INSERT INTO `compositeur_chanson` (`Atriste_ID`, `Chanson_ID`) VALUES
+INSERT INTO `compositeur_chanson` (`Artiste_ID`, `Chanson_ID`) VALUES
 (1, 5),
 (2, 6);
 
@@ -192,17 +192,17 @@ INSERT INTO `compositeur_chanson` (`Atriste_ID`, `Chanson_ID`) VALUES
 
 CREATE TABLE IF NOT EXISTS `conducteur_album` (
   `Album_ID` bigint(20) NOT NULL,
-  `Atriste_ID` bigint(20) NOT NULL,
-  PRIMARY KEY (`Album_ID`,`Atriste_ID`),
-  UNIQUE KEY `ID_Conduit_IND` (`Album_ID`,`Atriste_ID`),
-  KEY `FKCon_Art_IND` (`Atriste_ID`)
+  `Artiste_ID` bigint(20) NOT NULL,
+  PRIMARY KEY (`Album_ID`,`Artiste_ID`),
+  UNIQUE KEY `ID_Conduit_IND` (`Album_ID`,`Artiste_ID`),
+  KEY `FKCon_Art_IND` (`Artiste_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `conducteur_album`
 --
 
-INSERT INTO `conducteur_album` (`Album_ID`, `Atriste_ID`) VALUES
+INSERT INTO `conducteur_album` (`Album_ID`, `Artiste_ID`) VALUES
 (1, 1),
 (2, 2);
 
@@ -214,15 +214,15 @@ INSERT INTO `conducteur_album` (`Album_ID`, `Atriste_ID`) VALUES
 
 CREATE TABLE IF NOT EXISTS `image` (
   `Image_ID` int(11) NOT NULL AUTO_INCREMENT,
-  `url` varchar(500) CHARACTER SET utf8 NOT NULL,
-  `Atriste_ID` bigint(20) DEFAULT NULL,
+  `url` varchar(500) CHARACTER SET utf8 NOT NULL DEFAULT '27-07-2015-16-56-33_70ee7dd88fd1845ba53d844243ecf972_Les_minions.jpg.jpg',
+  `Artiste_ID` bigint(20) DEFAULT NULL,
   `Categorie_ID` bigint(20) DEFAULT NULL,
   `Label_ID` bigint(20) DEFAULT NULL,
   `Chanson_ID` bigint(20) DEFAULT NULL,
   `Album_ID` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`Image_ID`),
   UNIQUE KEY `ID_Image_IND` (`Image_ID`),
-  KEY `FKimage_artiste_IND` (`Atriste_ID`),
+  KEY `FKimage_artiste_IND` (`Artiste_ID`),
   KEY `FKimage_categorie_IND` (`Categorie_ID`),
   KEY `FKimage_label_IND` (`Label_ID`),
   KEY `FKimage_chanson_IND` (`Chanson_ID`),
@@ -237,17 +237,17 @@ CREATE TABLE IF NOT EXISTS `image` (
 
 CREATE TABLE IF NOT EXISTS `interprete_album` (
   `Album_ID` bigint(20) NOT NULL,
-  `Atriste_ID` bigint(20) NOT NULL,
-  PRIMARY KEY (`Album_ID`,`Atriste_ID`),
-  UNIQUE KEY `ID_InterpreteAlbum_IND` (`Album_ID`,`Atriste_ID`),
-  KEY `FKInt_Art_IND` (`Atriste_ID`)
+  `Artiste_ID` bigint(20) NOT NULL,
+  PRIMARY KEY (`Album_ID`,`Artiste_ID`),
+  UNIQUE KEY `ID_InterpreteAlbum_IND` (`Album_ID`,`Artiste_ID`),
+  KEY `FKInt_Art_IND` (`Artiste_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `interprete_album`
 --
 
-INSERT INTO `interprete_album` (`Album_ID`, `Atriste_ID`) VALUES
+INSERT INTO `interprete_album` (`Album_ID`, `Artiste_ID`) VALUES
 (1, 1),
 (2, 2);
 
@@ -258,19 +258,20 @@ INSERT INTO `interprete_album` (`Album_ID`, `Atriste_ID`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `interprete_chanson` (
-  `Atriste_ID` bigint(20) NOT NULL,
+  `Artiste_ID` bigint(20) NOT NULL,
   `Chanson_ID` bigint(20) NOT NULL,
-  PRIMARY KEY (`Chanson_ID`,`Atriste_ID`),
-  UNIQUE KEY `ID_Interprete_IND` (`Chanson_ID`,`Atriste_ID`),
-  KEY `FKInt_Art_1_IND` (`Atriste_ID`)
+  PRIMARY KEY (`Chanson_ID`,`Artiste_ID`),
+  UNIQUE KEY `ID_Interprete_IND` (`Chanson_ID`,`Artiste_ID`),
+  KEY `FKInt_Art_1_IND` (`Artiste_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `interprete_chanson`
 --
 
-INSERT INTO `interprete_chanson` (`Atriste_ID`, `Chanson_ID`) VALUES
+INSERT INTO `interprete_chanson` (`Artiste_ID`, `Chanson_ID`) VALUES
 (1, 5),
+(2, 5),
 (2, 6);
 
 -- --------------------------------------------------------
@@ -301,18 +302,18 @@ INSERT INTO `label` (`Label_ID`, `Nom`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `parolier_chanson` (
-  `Atriste_ID` bigint(20) NOT NULL,
+  `Artiste_ID` bigint(20) NOT NULL,
   `Chanson_ID` bigint(20) NOT NULL,
-  PRIMARY KEY (`Chanson_ID`,`Atriste_ID`),
-  UNIQUE KEY `ID_Ecrit_IND` (`Chanson_ID`,`Atriste_ID`),
-  KEY `FKEcr_Art_IND` (`Atriste_ID`)
+  PRIMARY KEY (`Chanson_ID`,`Artiste_ID`),
+  UNIQUE KEY `ID_Ecrit_IND` (`Chanson_ID`,`Artiste_ID`),
+  KEY `FKEcr_Art_IND` (`Artiste_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `parolier_chanson`
 --
 
-INSERT INTO `parolier_chanson` (`Atriste_ID`, `Chanson_ID`) VALUES
+INSERT INTO `parolier_chanson` (`Artiste_ID`, `Chanson_ID`) VALUES
 (1, 5),
 (2, 6);
 
@@ -360,13 +361,13 @@ ALTER TABLE `chanson_album`
 --
 ALTER TABLE `compositeur_album`
   ADD CONSTRAINT `FKCom_Alb` FOREIGN KEY (`Album_ID`) REFERENCES `album` (`Album_ID`),
-  ADD CONSTRAINT `FKCom_Art_FK` FOREIGN KEY (`Atriste_ID`) REFERENCES `artiste` (`Atriste_ID`);
+  ADD CONSTRAINT `FKCom_Art_FK` FOREIGN KEY (`Artiste_ID`) REFERENCES `artiste` (`Artiste_ID`);
 
 --
 -- Contraintes pour la table `compositeur_chanson`
 --
 ALTER TABLE `compositeur_chanson`
-  ADD CONSTRAINT `FKCom_Art_1_FK` FOREIGN KEY (`Atriste_ID`) REFERENCES `artiste` (`Atriste_ID`),
+  ADD CONSTRAINT `FKCom_Art_1_FK` FOREIGN KEY (`Artiste_ID`) REFERENCES `artiste` (`Artiste_ID`),
   ADD CONSTRAINT `FKCom_Cha` FOREIGN KEY (`Chanson_ID`) REFERENCES `chanson` (`Chanson_ID`);
 
 --
@@ -374,14 +375,14 @@ ALTER TABLE `compositeur_chanson`
 --
 ALTER TABLE `conducteur_album`
   ADD CONSTRAINT `FKCon_Alb` FOREIGN KEY (`Album_ID`) REFERENCES `album` (`Album_ID`),
-  ADD CONSTRAINT `FKCon_Art_FK` FOREIGN KEY (`Atriste_ID`) REFERENCES `artiste` (`Atriste_ID`);
+  ADD CONSTRAINT `FKCon_Art_FK` FOREIGN KEY (`Artiste_ID`) REFERENCES `artiste` (`Artiste_ID`);
 
 --
 -- Contraintes pour la table `image`
 --
 ALTER TABLE `image`
   ADD CONSTRAINT `FKimage_album_FK` FOREIGN KEY (`Album_ID`) REFERENCES `album` (`Album_ID`),
-  ADD CONSTRAINT `FKimage_artiste_FK` FOREIGN KEY (`Atriste_ID`) REFERENCES `artiste` (`Atriste_ID`),
+  ADD CONSTRAINT `FKimage_artiste_FK` FOREIGN KEY (`Artiste_ID`) REFERENCES `artiste` (`Artiste_ID`),
   ADD CONSTRAINT `FKimage_categorie_FK` FOREIGN KEY (`Categorie_ID`) REFERENCES `categorie` (`Categorie_ID`),
   ADD CONSTRAINT `FKimage_chanson_FK` FOREIGN KEY (`Chanson_ID`) REFERENCES `chanson` (`Chanson_ID`),
   ADD CONSTRAINT `FKimage_label_FK` FOREIGN KEY (`Label_ID`) REFERENCES `label` (`Label_ID`);
@@ -391,20 +392,20 @@ ALTER TABLE `image`
 --
 ALTER TABLE `interprete_album`
   ADD CONSTRAINT `FKInt_Alb` FOREIGN KEY (`Album_ID`) REFERENCES `album` (`Album_ID`),
-  ADD CONSTRAINT `FKInt_Art_FK` FOREIGN KEY (`Atriste_ID`) REFERENCES `artiste` (`Atriste_ID`);
+  ADD CONSTRAINT `FKInt_Art_FK` FOREIGN KEY (`Artiste_ID`) REFERENCES `artiste` (`Artiste_ID`);
 
 --
 -- Contraintes pour la table `interprete_chanson`
 --
 ALTER TABLE `interprete_chanson`
-  ADD CONSTRAINT `FKInt_Art_1_FK` FOREIGN KEY (`Atriste_ID`) REFERENCES `artiste` (`Atriste_ID`),
+  ADD CONSTRAINT `FKInt_Art_1_FK` FOREIGN KEY (`Artiste_ID`) REFERENCES `artiste` (`Artiste_ID`),
   ADD CONSTRAINT `FKInt_Cha` FOREIGN KEY (`Chanson_ID`) REFERENCES `chanson` (`Chanson_ID`);
 
 --
 -- Contraintes pour la table `parolier_chanson`
 --
 ALTER TABLE `parolier_chanson`
-  ADD CONSTRAINT `FKEcr_Art_FK` FOREIGN KEY (`Atriste_ID`) REFERENCES `artiste` (`Atriste_ID`),
+  ADD CONSTRAINT `FKEcr_Art_FK` FOREIGN KEY (`Artiste_ID`) REFERENCES `artiste` (`Artiste_ID`),
   ADD CONSTRAINT `FKEcr_Cha` FOREIGN KEY (`Chanson_ID`) REFERENCES `chanson` (`Chanson_ID`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
