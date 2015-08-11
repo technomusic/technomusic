d1185c08b5255ba9c16752026808f3ea697e35f1
loicpayen <loicpayen@hotmail.com>
2015-08-11 14:45:26 +0200

    commit afiche label list

--- technomusic/index.php
+++ technomusic/index.php
@@ -7,38 +7,16 @@ include('lib/file.inc.php');
 include 'views/header-page-bootsrap.inc.php';
 
 //inclusion du menu
-include 'views/menu-inc.php' ;
+//include 'views/menu-inc.php' ;
 
 //on vérifie qui nous appel
 if (isset($_REQUEST["section"])) {
     switch ($_REQUEST["section"]) {
-        case "affiche-movie-exec":
-            //include("mods/movies.inc.php");
-            include("views/movie-uniq-bootstrap.inc.php");
+        case "affiche-label-liste":
+            include("views/label-liste.inc.php");
             break;
-        case "search-movie-form":
-            include("views/movie-searchform-bootstrap.inc.php");
-            break;
-        case "search-movie-exec":
-            //include("mods/movies.inc.php");
-            include("views/searchmovies-list-bootstrap.inc.php");
-            break;
-        case "insert-movie-form":
-            include("views/movie-insertform-bootstrap.inc.php");
-            break;
-        case "insert-movie-exec":
-            include("mods/movies.inc.php");
-            //include("views/cars-confirm-bootstrap.inc.php");
-            break;
-        case "delete-movie-exec":
-            include("mods/movies.inc.php");
-            //include("views/cars-confirm-bootstrap.inc.php");
-            break;
-        case "update-movie-form":
-            include("views/movie-updateform-bootstrap.inc.php");
-            break;
-        case "update-movie-exec":
-            include("mods/movies.inc.php");
+        case "affiche-label-unic":
+            include("views/label-unic.inc.php");
             break;
     }
 //si personne ne nous a appelé c'est qu'on arrive pour la première fois. On affiche donc la page d'accueil
--- technomusic/views/label-liste.inc.php
+++ technomusic/views/label-liste.inc.php
@@ -1,5 +1,6 @@
 <div class="bs-component">
     <table class="table table-striped table-hover ">
+        
 
 <?php
 
@@ -8,14 +9,14 @@
         $dbh = new PDO("mysql:host=$hostname;dbname=$dbname",   $username, $password);
         //$dbh = new PDO("sqlite:./data/movies.db");
         $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $sql = "SELECT label.Label_ID AS id, label.Label_Nom AS Nom, JOIN image ON image_label.Label_ID=image.Label_ID";
+        $sql = "SELECT label.Label_ID AS id, label.Nom AS nom FROM label JOIN image ON label.Label_ID=image.Label_ID";
         $stmt = $dbh->query($sql);
-        $row = $stmt->fetch(PDO::FETCH_ASSOC);
-        $id = $row["id"];
-        $nom = $row["nom"];
         while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
-        {    
-           echo "<tr><td><a href=\"?section=affiche-label-exec&id=" . $id . "\">" . $nom . "</a></td></tr>";
+        {
+            
+            $id = $row["id"];
+            $nom = $row["nom"];
+            echo "<tr><td><a href=\"?section=affiche-label-unic&id=" . $id . "\">" . $nom . "</a></td></tr>";
         }
         unset($dbh);
     }
