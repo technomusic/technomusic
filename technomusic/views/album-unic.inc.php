cb32c16c7fc7b47347aa3ae34e04efbd16f510a5
Flink88 <meanf.isat+github@gmail.com>
2015-08-13 15:05:41 +0200

    correction album-unic.inc.php

--- technomusic/views/album-unic.inc.php
+++ technomusic/views/album-unic.inc.php
@@ -7,28 +7,12 @@
         $dbh = new PDO("mysql:host=$hostname;dbname=$dbname",   $username, $password);
         //$dbh = new PDO("sqlite:./data/movies.db");
         $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+        $dbh->query('SET NAMES utf8');
         $sql = "SELECT album.Album_ID AS id, album.Titre AS titre, album.Annee AS annee, chanson.Titre AS nomchanson, label.Nom as label, artisteinterprete.Nom AS nominter, artistecompositeur.Nom AS nomcompo, artisteconducteur.Nom AS nomconduct, image.url AS image FROM album JOIN interprete_album ON album.Album_ID=interprete_album.Album_ID JOIN artiste AS artisteinterprete ON interprete_album.Artiste_ID=artisteinterprete.Artiste_ID JOIN label ON album.Label_ID=label.Label_ID JOIN compositeur_album ON album.Album_ID=compositeur_album.Album_ID JOIN artiste AS artistecompositeur ON compositeur_album.Artiste_ID=artistecompositeur.Artiste_ID JOIN conducteur_album ON album.Album_ID=conducteur_album.Album_ID JOIN artiste AS artisteconducteur ON conducteur_album.Artiste_ID=artisteconducteur.Artiste_ID JOIN chanson_album ON album.Album_ID=chanson_album.Album_ID JOIN chanson ON chanson_album.Chanson_ID=chanson.Chanson_ID JOIN image ON album.Album_ID=image.Album_ID WHERE album.Album_ID=:id";
         $stmt = $dbh->prepare($sql);
         $stmt->bindValue("id", $_REQUEST["id"]);
         $stmt->execute();
-        
-        $id;
-        $titre;
-        $annee;
-        $label;
-        $image;
-        $nomchanson;
-        $nominter;
-        $nomcompo;
-        $nomconduct;
-        $artisteinterprete;
-        $artistecompositeur;
-        $artisteconducteur;
-        
-        
-        while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
-        {
-            
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
         $id = $row["id"];
         $titre = $row["titre"];
         $annee = $row["annee"];
@@ -45,6 +29,12 @@
         $txtcompositeur = "";
         $txtconducteur = "";
         $txtchanson = "";
+        
+        
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
+        {
+            
+
         if ($id == $row["id"]) {
                     $nominter[] = $row["nominter"];
                     $nomcompo[] = $row["nomcompo"];
