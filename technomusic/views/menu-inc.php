e8119882fafa9b7c2532902adc2b095153196ebc
Flink88 <meanf.isat+github@gmail.com>
2015-08-13 11:42:42 +0200

    correction menu suite merge

--- technomusic/views/menu-inc.php
+++ technomusic/views/menu-inc.php
@@ -1,18 +1,12 @@
 <div class="menu" name="menu">
     <div class="men">
         <table class ="tab-menu">
-<<<<<<< HEAD
         <tr><td><a href="?section=search-movie-form"><div class ="men-td">Recherche</div></a></td></tr>
-        <tr><td><a href="?section=affiche-artiste-liste"><div class ="men-td">atiste</div></a></td></tr>
+        <tr><td><a href="?section=affiche-artiste-liste"><div class ="men-td">artiste</div></a></td></tr>
         <tr><td><a href="?section=affiche-album-liste"><div class ="men-td">album</div></a></td></tr>
-=======
->>>>>>> origin/master
         <tr><td><a href="?section=affiche-chanson-liste"><div class ="men-td">chansons</div></a></td></tr>
-        <tr><td><a href="?section=affiche-album-liste"><div class ="men-td">albums</div></a></td></tr>
-        <tr><td><a href="?section=affiche-artiste-liste"><div class ="men-td">artistes</div></a></td></tr>
         <tr><td><a href="?section=affiche-categorie-liste"><div class ="men-td">categories</div></a></td></tr>
         <tr><td><a href="?section=affiche-label-liste"><div class ="men-td">labels</div></a></td></tr>
-        <tr><td><a href="?section=search-form"><div class ="men-td">Rechercher</div></a></td></tr>
     </table>
     </div>
 </div>
