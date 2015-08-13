e3248e2d69814dbc3b5b635c114b09a76c4a2936
Flink88 <meanf.isat+github@gmail.com>
2015-08-13 11:44:52 +0200

    rectification mauvais changements suite à merge

--- technomusic/views/menu-inc.php
+++ technomusic/views/menu-inc.php
@@ -1,12 +1,12 @@
 <div class="menu" name="menu">
     <div class="men">
         <table class ="tab-menu">
-        <tr><td><a href="?section=search-movie-form"><div class ="men-td">Recherche</div></a></td></tr>
-        <tr><td><a href="?section=affiche-artiste-liste"><div class ="men-td">artiste</div></a></td></tr>
-        <tr><td><a href="?section=affiche-album-liste"><div class ="men-td">album</div></a></td></tr>
+        <tr><td><a href="?section=affiche-artiste-liste"><div class ="men-td">artistes</div></a></td></tr>
         <tr><td><a href="?section=affiche-chanson-liste"><div class ="men-td">chansons</div></a></td></tr>
+        <tr><td><a href="?section=affiche-album-liste"><div class ="men-td">albums</div></a></td></tr>
         <tr><td><a href="?section=affiche-categorie-liste"><div class ="men-td">categories</div></a></td></tr>
         <tr><td><a href="?section=affiche-label-liste"><div class ="men-td">labels</div></a></td></tr>
+        <tr><td><a href="?section=search-movie-form"><div class ="men-td">Rechercher</div></a></td></tr>
     </table>
     </div>
 </div>
