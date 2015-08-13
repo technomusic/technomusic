9e4640e4cc9cf3b26d88df298a0b206c214cf10b
Flink88 <meanf.isat+github@gmail.com>
2015-08-13 15:45:39 +0200

    correctif artiste-unic.inc.php

--- technomusic/views/artiste-unic.inc.php
+++ technomusic/views/artiste-unic.inc.php
@@ -8,7 +8,7 @@
         //$dbh = new PDO("sqlite:./data/movies.db");
         $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
         $dbh->query('SET NAMES utf8');
-        $sql = "SELECT artiste.Artiste_ID AS id, artiste.Bio as bio, artiste.Nom AS nom, artiste.Prenom AS prenom, artiste.Surnom AS surnom, artiste.Lieu_Naissance AS lieu, DATE_FORMAT(artiste.Date_Naissance,'%d/%m/%Y') AS dn, image.url AS image FROM artiste JOIN image ON artiste.Artiste_ID=image.Artiste_ID WHERE artiste.Artiste_ID=:id";
+        $sql = "SELECT artiste.Artiste_ID AS id, artiste.Bio as bio, artiste.Nom AS nom, artiste.Prenom AS prenom, artiste.Surnom AS surnom, artiste.Lieu_Naissance AS lieu, DATE_FORMAT(artiste.Date_Naissance,'%d/%m/%Y') AS dn, image.url AS image FROM artiste LEFT JOIN image ON artiste.Artiste_ID=image.Artiste_ID WHERE artiste.Artiste_ID=:id";
         $stmt = $dbh->prepare($sql);
         $stmt->bindValue("id", $_REQUEST["id"]);
         $stmt->execute();
@@ -30,11 +30,23 @@
             else {
                 $surnom = "";
             }
+            if(isset($row["dn"])){
+                $dn = "Né le " . $row["dn"] . " ";
+            }
+            else {
+                $dn = "";
+            }
+            if(isset($row["lieu"])){
+                $lieu = "à " . $row["lieu"] . " ";
+            }
+            else {
+                $lieu = "";
+            }
         }   
         
         unset($dbh);
         $image = "data/images/" . $image;
-        echo "<div class=\"panel-heading\"><h3 class=\"panel-title\">" . $nom . "&nbsp" . $surnom . "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp" . $prenom . "<a href=\"?section=update-movie-form&id=" . $id . "\" class=\"btn btn-warning\">Moddifier</a><a href=\"?section=delete-movie-exec&id=" . $id . "\" class=\"btn btn-danger\">Supprimer</a></h3></div><div>Né le " . $dn . " à " . $lieu . "</div><div><img ";?> <?php fctaffichimage($image, 200, 200) ?> <?php echo "/\"></div><div><h5>" . $bio . "</h5></div>";
+        echo "<div class=\"panel-heading\"><h3 class=\"panel-title\">" . $nom . "&nbsp" . $surnom . "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp" . $prenom . "<a href=\"?section=update-movie-form&id=" . $id . "\" class=\"btn btn-warning\">Moddifier</a><a href=\"?section=delete-movie-exec&id=" . $id . "\" class=\"btn btn-danger\">Supprimer</a></h3></div><div>" . $dn . $lieu . "</div><div><img ";?> <?php fctaffichimage($image, 200, 200) ?> <?php echo "/\"></div><div><h5>" . $bio . "</h5></div>";
     }
     catch(PDOException $e)
     {
