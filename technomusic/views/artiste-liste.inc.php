beb31a43a63e2d94c3f03dadbb7023b5e48da2e2
Flink88 <meanf.isat+github@gmail.com>
2015-08-13 10:54:49 +0200

    pré final artiste-liste.inc.php

--- technomusic/views/artiste-liste.inc.php
+++ technomusic/views/artiste-liste.inc.php
@@ -1,30 +1,29 @@
 <div class="bs-component">
     <table class="table table-striped table-hover ">
-        
 
-<?php
-echo "<thead><tr><th>Nom</th></tr></thead><tbody>";
-    try
-    {
-        $dbh = new PDO("mysql:host=$hostname;dbname=$dbname",   $username, $password);
-        //$dbh = new PDO("sqlite:./data/movies.db");
-        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $sql = "SELECT artiste.Artiste_ID AS id, artiste.Nom AS nom FROM artiste JOIN image ON artiste.Artiste_ID=image.Artiste_ID";
-        $stmt = $dbh->query($sql);
-        while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
-        {
-            $id = $row["id"];
-            $nom = $row["nom"];
-            echo "<tr><td><a href=\"?section=affiche-artiste-unic&id=" . $id . "\">" . $nom . "</a></td></tr>";
+
+        <?php
+        echo "<thead><tr><th>Nom</th><th>Prénom</th><th>Pseudo</th><th>Date de naissance</th></tr></thead><tbody>";
+        try {
+            $dbh = new PDO("mysql:host=$hostname;dbname=$dbname", $username, $password);
+            //$dbh = new PDO("sqlite:./data/movies.db");
+            $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $sql = "SELECT artiste.Artiste_ID AS id, artiste.Nom AS nom, artiste.Prenom AS prenom, artiste.Surnom AS pseudo, DATE_FORMAT(artiste.Date_Naissance,'%d/%m/%Y') AS dn FROM artiste LEFT JOIN image ON artiste.Artiste_ID=image.Artiste_ID";
+            $stmt = $dbh->query($sql);
+            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                $id = $row["id"];
+                $nom = $row["nom"];
+                $prenom = $row["prenom"];
+                $pseudo = $row["pseudo"];
+                $dn = $row["dn"];
+
+                echo "<tr><td><a href=\"?section=affiche-artiste-unic&id=" . $id . "\">" . $nom . "</a></td><td>" . $prenom . "</td><td>" . $pseudo . "</td><td>" . $dn . "</td></tr>";
+            }
+            unset($dbh);
+        } catch (PDOException $e) {
+            echo $e->getMessage();
         }
-        unset($dbh);
-    }
-    catch(PDOException $e)
-    {
-        echo $e->getMessage();
-    } 
-
-?>
+        ?>
 
     </table>
 </div>
