c9f4bec6de2a1c0f79a057716a5cdc3c71878932
Flink88 <meanf.isat+github@gmail.com>
2015-08-14 09:02:11 +0200

    fin chanson et album unic avant css

--- technomusic/views/chanson-unic.inc.php
+++ technomusic/views/chanson-unic.inc.php
@@ -7,12 +7,14 @@
             //$dbh = new PDO("sqlite:./data/movies.db");
             $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $dbh->query('SET NAMES utf8');
-            $sql = "SELECT chanson.Chanson_ID AS id, chanson.Titre AS titre, chanson.Annee AS annee, chanson.Duree AS duree, categorie.Nom AS nomcat, artisteinterprete.Nom AS nominter, artistecompositeur.Nom AS nomcompo, artisteparolier.Nom AS nomparo, image.url AS image FROM chanson LEFT JOIN interprete_chanson ON chanson.Chanson_ID=interprete_chanson.Chanson_ID LEFT JOIN artiste AS artisteinterprete ON interprete_chanson.Artiste_ID=artisteinterprete.Artiste_ID LEFT JOIN categorie ON chanson.Categorie_ID=categorie.Categorie_ID LEFT JOIN compositeur_chanson ON chanson.Chanson_ID=compositeur_chanson.Chanson_ID LEFT JOIN artiste AS artistecompositeur ON compositeur_chanson.Artiste_ID=artistecompositeur.Artiste_ID LEFT JOIN parolier_chanson ON chanson.Chanson_ID=parolier_chanson.Chanson_ID LEFT JOIN artiste AS artisteparolier ON parolier_chanson.Artiste_ID=artisteparolier.Artiste_ID ORDER BY titre JOIN image ON chanson.Chanson_ID=image.Chanson_ID";
-            $stmt = $dbh->query($sql);
+            $sql = "SELECT chanson.Chanson_ID AS id, chanson.Titre AS titre, chanson.Annee AS annee, chanson.Duree AS duree, categorie.Nom AS nomcat, artisteinterprete.Nom AS nominter, artistecompositeur.Nom AS nomcompo, artisteparolier.Nom AS nomparo, image.url AS image FROM chanson LEFT JOIN interprete_chanson ON chanson.Chanson_ID=interprete_chanson.Chanson_ID LEFT JOIN artiste AS artisteinterprete ON interprete_chanson.Artiste_ID=artisteinterprete.Artiste_ID LEFT JOIN categorie ON chanson.Categorie_ID=categorie.Categorie_ID LEFT JOIN compositeur_chanson ON chanson.Chanson_ID=compositeur_chanson.Chanson_ID LEFT JOIN artiste AS artistecompositeur ON compositeur_chanson.Artiste_ID=artistecompositeur.Artiste_ID LEFT JOIN parolier_chanson ON chanson.Chanson_ID=parolier_chanson.Chanson_ID LEFT JOIN artiste AS artisteparolier ON parolier_chanson.Artiste_ID=artisteparolier.Artiste_ID JOIN image ON chanson.Chanson_ID=image.Chanson_ID WHERE chanson.Chanson_ID=:id";
+            $stmt = $dbh->prepare($sql);
+            $stmt->bindValue("id", $_REQUEST["id"]);
+            $stmt->execute();
             $row = $stmt->fetch(PDO::FETCH_ASSOC);
             $id = $row["id"];
             $titre = $row["titre"];
-            $cat = $row["nomcat"];
+            $nomcat = $row["nomcat"];
             $image = $row["image"];
             $interprete = array($row["nominter"]);
             $compositeur = array($row["nomcompo"]);
@@ -23,72 +25,9 @@
             $txtcompositeur = "";
             $txtparolier = "";
             while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                if ($id == $row["id"]) {
-                    $interprete[] = $row["nominter"];
-                    $compositeur[] = $row["nomcompo"];
-                    $parolier[] = $row["nomparo"];
-                    //quand ce n'est plus le même id...
-                } else {
-                    //... on élimine les doublons interprete...
-                    $result = array_unique($interprete);
-                    //... on change de tableau pour réinitialiser les clefs...
-                    $tab = array();
-                    foreach ($result as $value) {
-                        $tab[] = $value;
-                    }
-                    //... on calcul la taille...
-                    $size = sizeof($tab);
-                    //... on ecrit le premier...
-                    $txtinterprete = $tab[0];
-                    //...ensuite on écrit tout les autres...
-                    for ($i = 1; $i < $size; $i++) {
-                        $txtinterprete = $txtinterprete . "<br/>" . $tab[$i];
-                    }
-                    //...pour enfin netoyer le tableau pour les autre albums...
-                    $interprete = array();
-
-                    //idem interprete
-                    $result = array_unique($compositeur);
-                    $tab = array();
-                    foreach ($result as $value) {
-                        $tab[] = $value;
-                    }
-                    $size = sizeof($tab);
-                    $txtcompositeur = $tab[0];
-                    for ($i = 1; $i < $size; $i++) {
-                        $txtcompositeur = $txtcompositeur . "<br/>" . $tab[$i];
-                    }
-                    $compositeur = array();
-
-                    //idem interprete
-                    $result = array_unique($parolier);
-                    $tab = array();
-                    foreach ($result as $value) {
-                        $tab[] = $value;
-                    }
-                    $size = sizeof($tab);
-                    $txtparolier = $tab[0];
-                    for ($i = 1; $i < $size; $i++) {
-                        $txtparolier = $txtparolier . "<br/>" . $tab[$i];
-                    }
-                    $parolier = array();
-
-                    $image = "data/images/" . $image;
-                    //fin de journée ici
-                    echo "<div class=\"panel-heading\"><h3 class=\"panel-title\">" . $titre . "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp(" . $annee . ")<a href=\"?section=update-movie-form&id=" . $id . "\" class=\"btn btn-warning\">Moddifier</a><a href=\"?section=delete-movie-exec&id=" . $id . "\" class=\"btn btn-danger\">Supprimer</a></h3></div><div class=\"panel-body\"><h3>" . $duree . " secondes&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp" . $nomcat . "</h3><div>" . $nomart . "</div><div class='im-chanson-unique'><img "; ?> <?php fctaffichimage($image, 200, 200) ?> <?php echo "/\"></div></div>";
-                    $id = $row["id"];
-                    $titre = $row["titre"];
-                    $cat = $row["nomcat"];
-                    $image = $row["image"];
-                    $interprete = array($row["nominter"]);
-                    $compositeur = array($row["nomcompo"]);
-                    $parolier = array($row["nomparo"]);
-                    $annee = $row["annee"];
-                    $duree = $row["duree"];
-                    $txtinterprete = "";
-                    $txtcompositeur = "";
-                    $txtparolier = "";
-                }
+                $interprete[] = $row["nominter"];
+                $compositeur[] = $row["nomcompo"];
+                $parolier[] = $row["nomparo"];
             }
             //... on élimine les doublons interprete...
             $result = array_unique($interprete);
@@ -134,7 +73,10 @@
             }
             $parolier = array();
 
-            echo "<tr><td><a href=\"?section=affiche-chanson-unic&id=" . $id . "\">" . $titre . "</a></td><td>" . $annee . "</td><td>" . $duree . "</td><td>" . $cat . "</td><td>" . $txtinterprete . "</td><td>" . $txtcompositeur . "</td><td>" . $txtparolier . "</td></tr>";
+            $image = "data/images/" . $image;
+            echo "<div class=\"panel-heading\"><h3 class=\"panel-title\">" . $titre . "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp(" . $annee . ")<a href=\"?section=update-movie-form&id=" . $id . "\" class=\"btn btn-warning\">Moddifier</a><a href=\"?section=delete-movie-exec&id=" . $id . "\" class=\"btn btn-danger\">Supprimer</a></h3></div><div class=\"panel-body\"><h3>" . $duree . " secondes&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp" . $nomcat . "</h3><div><h4>Interprète(s) : </h4><h5>" . $txtinterprete . "</h5> </div><div><h4>Compositeur : </h4><h5>" . $txtcompositeur . "</h5></div><div><h4>Parolier(s) : </h4><h5>" . $txtparolier . "</h5></div><div class='im-chanson-unique'><img ";
+            ?> <?php fctaffichimage($image, 200, 200) ?> <?php
+            echo "/\"></div></div>";
             unset($dbh);
         } catch (PDOException $e) {
             echo $e->getMessage();
