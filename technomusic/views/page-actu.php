8bda5a8d54d573e771689e9e451dfcdabf0e69bd
Flink88 <meanf.isat+github@gmail.com>
2015-08-14 16:00:47 +0200

    PR2 MODIF PAGE ACTU

--- technomusic/views/page-actu.php
+++ technomusic/views/page-actu.php
@@ -1,17 +1,6 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <title>page d'accueil</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="design/css/actuStyles.css" type="text/css">
-    </head>
-    <body>
+
+
+
         <div class="acceui">
             <div class="actu1">
                 <div class="actu1-1">
@@ -70,5 +59,3 @@ and open the template in the editor.
 
             </div>
         </div>
-    </body>
-</html>
