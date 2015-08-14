9ad36bb0e1d8ef581a33ef6eefb5bb6d3a20f65a
phildel <surfez90@gmail.com>
2015-08-14 15:16:35 +0200

    page d'accueil
    mise en plce d'actus

--- technomusic/views/page-actu.php
+++ technomusic/views/page-actu.php
@@ -13,15 +13,62 @@ and open the template in the editor.
     </head>
     <body>
         <div class="acceui">
-            <div class="actu1"><div class="actu1-1">http://www.lesinrocks.com/inrocks.tv/aap-rocky-un-clip-retro-pour-jukebox-joints/</div><div class=actu1-2>
-                    Après le clip hallucinogène de L$D, A$AP Rocky est de retour avec Jukebox Joints, morceau issu de son album At.Long.Last
-                    .A$AP sorti en juin dernier. Si les stupéfiants sont toujours au programme, la vidéo commence cette...
-                </div></div>
-            <div class="actu2"></div>
-            <div class="actu3"></div>
-            <div class="actu4"></div>
-            <div class="actu5"></div>
-            <div class="actu6"></div>
-        </div>
+            <div class="actu1">
+                <div class="actu1-1">
+                    <iframe class="frame" width="560" height="315" src="https://www.youtube.com/embed/UtZBA1bVbcs?list=PL2kicx1M78xUeXOfRLsfrDWFckyR-oy88" frameborder="0" allowfullscreen></iframe>
+             </div>
+                <div class=actu1-2>
+                      Après le clip hallucinogène de L$D, A$AP Rocky est de retour avec Jukebox Joints, morceau issu de son album At.Long.Last
+                        .A$AP sorti en juin dernier. Si les stupéfiants sont toujours au programme, la vidéo commence cette...</div>
+                </div>
+        <div class="actu1">
+           
+                <div class="actu1-1">
+                    <iframe class="frame" width="420" height="315" src="https://www.youtube.com/embed/dC06CMBQLM0" frameborder="0" allowfullscreen></iframe>
+                </div>
+                <div class="actu1-2">
+                    Rihanna célèbre cette année ses 10 ans de carrière. Alors que son huitième album se fait toujours désirer, la chanteuse a accepté d'être consultante
+                    dans la neuvième saison de "The Voice" aux Etats-Unis, auprès d'Adam Levine, Gwen Stefani, Blake Shelton, et Pharrell. ...
+                                   
+            </div>
+        </div>    
+            <div class="actu1">
+                <div class ="actu1-1">
+                    <iframe class="frame" width="560" height="315" src="https://www.youtube.com/embed/wSXPHNHuw7A" frameborder="0" allowfullscreen></iframe>
+                </div>
+                <div class="actu1-2">Actuellement en vacances aux Etats-Unis, après avoir terminé sa tournée acoustique, Tal multiplie les projets.
+                    Déjà égérie Tex, la chanteuse sortira en septembre sa propre collection de lingerie en partenariat avec la marque française 
+                </div>                    
+            </div>
+            <div class="actu1">
+                <div class="actu1-1">
+                    <iframe width="420" height="315" src="https://www.youtube.com/embed/YII3KXQ86F0" frameborder="0" allowfullscreen></iframe>
+                </div>
+                <div class="actu1-2">Attendu le 28 août, le double-album "Mon coeur avait raison" de Maître Gims contiendra notamment un duo avec... 
+                    Sia, intitulé "Je te pardonne". Le rappeur de la Sexion d'Assaut raconte la genèse de ce duo événement : "Elle m'a ..
+                </div>                   
+            </div>
+            <div class="actu1">
+                <div class="actu1-1">
+                 <iframe class="frame" width="420" height="315" src="https://www.youtube.com/embed/sVaRHJn_dgM" frameborder="0" allowfullscreen></iframe>   
+                </div>
+                <div class="actu1-2">
+                 Le coup d'envoi du Sziget Festival a été donné ce lundi 10 août à Budapest avec le concert très attendu du dandy anglais 
+                 Robbie Williams. Le chanteur a livré un show remarquable mêlant à la fois tubes de son répertoire et reprises de titres connus de tous.
+                 Retour sur cette soirée d'ouverture ...   
+                </div>
+                    
+            </div>
+            <div class="actu1">
+                <div class="actu1-1">
+                 <iframe class ="frame" width="560" height="315" src="https://www.youtube.com/embed/da1xoQFE7ts" frameborder="0" allowfullscreen></iframe>   
+                </div>
+                <div class="actu1-2">
+                 Mariah Carey compte bien faire de son single "Infinity" un tube. Pour mettre toutes les chances de son côté, la chanteuse a convié French Montana,
+                 Justin Bieber et T.I. sur "Why U Mad", un remix rap. Ecoutez ..   
+                </div>
+                    
+            </div>
+       </div>
     </body>
 </html>
