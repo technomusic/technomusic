cf78706e872a3e49909356eafd14848ad0ad312c
Flink88 <meanf.isat+github@gmail.com>
2015-08-14 13:18:06 +0200

    correction merge

--- technomusic/views/chanson-unic.inc.php
+++ technomusic/views/chanson-unic.inc.php
@@ -77,9 +77,6 @@
             echo "<div class=\"panel-heading\"><h3 class=\"panel-title\">" . $titre . "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp(" . $annee . ")<a href=\"?section=update-movie-form&id=" . $id . "\" class=\"btn btn-warning\">Moddifier</a><a href=\"?section=delete-movie-exec&id=" . $id . "\" class=\"btn btn-danger\">Supprimer</a></h3></div><div class=\"panel-body\"><h3>" . $duree . " secondes&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp" . $nomcat . "</h3><div><h4>Interprète(s) : </h4><h5>" . $txtinterprete . "</h5> </div><div><h4>Compositeur : </h4><h5>" . $txtcompositeur . "</h5></div><div><h4>Parolier(s) : </h4><h5>" . $txtparolier . "</h5></div><div class='im-chanson-unique'><img ";
             ?> <?php fctaffichimage($image, 200, 200) ?> <?php
             echo "/\"></div></div>";
-<<<<<<< HEAD
-            
-=======
             /* echo "
               <div class=\"panel-heading\">
               <div class='header-unic'><h3 class=\"panel-title\">" . $titre .
@@ -94,7 +91,6 @@
               ?> <?php fctaffichimage($image, 200, 200) ?><?php
               echo "/\"></div></div>"; */
 
->>>>>>> origin/master
             unset($dbh);
         } catch (PDOException $e) {
             echo $e->getMessage();
--- technomusic/views/recup-inscript.php
+++ technomusic/views/recup-inscript.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
 
 if ((isset($_POST['pseudo']) && !empty($_POST['pseudo'])) && (isset($_POST['password']) && !empty($_POST['password'])) && (isset($_POST['password1']) && !empty($_POST['password1']))) {
     if ($_POST['password'] != $_POST['password1']) {
@@ -24,40 +23,10 @@ if ((isset($_POST['pseudo']) && !empty($_POST['pseudo'])) && (isset($_POST['pass
                 $stmt->bindValue("password", md5($_REQUEST['password']));
                 $stmt->execute();
 
-=======
-
-if (isset($_POST['inscription']) && $_POST['inscription'] == 'inscription') {
-    if ((isset($_POST['login']) && !empty($_POST['login'])) && (isset($_POST['password']) && !empty($_POST['password'])) && (isset($_POST['password1']) && !empty($_POST['password1']))) {
-        if ($_POST[password] != $_POST['password1']) {
-            $e = 'les deux mots de passe sont différents ';
-        } else {
-
-
-            try {
-                $dbh = new PDO("mysql:host=$hostname;dbname=$dbname", $username, $password);
-                //$dbh = new PDO("sqlite:./data/movies.db");
-                $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                $dbh->query('SET NAMES utf8');
-                $sql = "SELECT pseudo FROM utilisateur WHERE pseudo = ($_POST['pseudo'])";
-                $req = mysql_query($sql);
-                $data = mysqi_fetch_array($req);
-                if ($data == 0) {
-                    $sql = "INSERT INTO utilisateur ( Pseudo , Nom ,prenom , Password )VALUE (:pseudo , :nom , :prenom ,:password )";
-                    $stmt = $dbh->prepare($sql);
-                    $stmt->bindValue("pseudo", $_REQUEST['pseudo']);
-                    $stmt->bindValue("nom", $_REQUEST['nom']);
-                    $stmt->bindValue("prenom", $_REQUEST['prenom']);
-                    $stmt->bindValue("password", md5($_REQUEST['password']));
-                    $stmt->execute();
-                }
-
-                unset($dbh);
->>>>>>> origin/master
                 echo " <div><h2>Voici les données que vous avez rentrée</h2>
             <div>votre nom :" . $_REQUEST['nom'] . "</div>
                 <div>Votre prenom :" . $_REQUEST['prenom'] . "</div>
             <div>Votre pseudo :" . $_REQUEST['pseudo'] . "</div>
-<<<<<<< HEAD
                 <div><a href='index.php' >Retour à l'accueil</a></div> ";
             } else {
                 echo "Ce pseudo est déjà utilisé";
@@ -66,12 +35,6 @@ if (isset($_POST['inscription']) && $_POST['inscription'] == 'inscription') {
             unset($dbh);
         } catch (PDOException $e) {
             echo $e->getMessage();
-=======
-                <div><a href='index.php' >veuillez confirmer . merci </a></div> ";
-            } catch (PDOException $e) {
-                echo $e->getMessage();
-            }
->>>>>>> origin/master
         }
     }
 }
