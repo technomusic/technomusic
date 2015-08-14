8a9acbc1f352083ab748bc928a6d85e41431e93c
Flink88 <meanf.isat+github@gmail.com>
2015-08-14 15:52:08 +0200

    commit page actu

--- technomusic/views/page-actu.php
+++ technomusic/views/page-actu.php
@@ -13,26 +13,25 @@ and open the template in the editor.
     </head>
     <body>
         <div class="acceui">
-<<<<<<< HEAD
             <div class="actu1">
                 <div class="actu1-1">
                     <iframe class="frame" width="560" height="315" src="https://www.youtube.com/embed/UtZBA1bVbcs?list=PL2kicx1M78xUeXOfRLsfrDWFckyR-oy88" frameborder="0" allowfullscreen></iframe>
-             </div>
-                <div class=actu1-2>
-                      Après le clip hallucinogène de L$D, A$AP Rocky est de retour avec Jukebox Joints, morceau issu de son album At.Long.Last
-                        .A$AP sorti en juin dernier. Si les stupéfiants sont toujours au programme, la vidéo commence cette...</div>
                 </div>
-        <div class="actu1">
-           
+                <div class=actu1-2>
+                    Après le clip hallucinogène de L$D, A$AP Rocky est de retour avec Jukebox Joints, morceau issu de son album At.Long.Last
+                    .A$AP sorti en juin dernier. Si les stupéfiants sont toujours au programme, la vidéo commence cette...</div>
+            </div>
+            <div class="actu1">
+
                 <div class="actu1-1">
                     <iframe class="frame" width="420" height="315" src="https://www.youtube.com/embed/dC06CMBQLM0" frameborder="0" allowfullscreen></iframe>
                 </div>
                 <div class="actu1-2">
                     Rihanna célèbre cette année ses 10 ans de carrière. Alors que son huitième album se fait toujours désirer, la chanteuse a accepté d'être consultante
                     dans la neuvième saison de "The Voice" aux Etats-Unis, auprès d'Adam Levine, Gwen Stefani, Blake Shelton, et Pharrell. ...
-                                   
-            </div>
-        </div>    
+
+                </div>
+            </div>    
             <div class="actu1">
                 <div class ="actu1-1">
                     <iframe class="frame" width="560" height="315" src="https://www.youtube.com/embed/wSXPHNHuw7A" frameborder="0" allowfullscreen></iframe>
@@ -51,37 +50,25 @@ and open the template in the editor.
             </div>
             <div class="actu1">
                 <div class="actu1-1">
-                 <iframe class="frame" width="420" height="315" src="https://www.youtube.com/embed/sVaRHJn_dgM" frameborder="0" allowfullscreen></iframe>   
+                    <iframe class="frame" width="420" height="315" src="https://www.youtube.com/embed/sVaRHJn_dgM" frameborder="0" allowfullscreen></iframe>   
                 </div>
                 <div class="actu1-2">
-                 Le coup d'envoi du Sziget Festival a été donné ce lundi 10 août à Budapest avec le concert très attendu du dandy anglais 
-                 Robbie Williams. Le chanteur a livré un show remarquable mêlant à la fois tubes de son répertoire et reprises de titres connus de tous.
-                 Retour sur cette soirée d'ouverture ...   
+                    Le coup d'envoi du Sziget Festival a été donné ce lundi 10 août à Budapest avec le concert très attendu du dandy anglais 
+                    Robbie Williams. Le chanteur a livré un show remarquable mêlant à la fois tubes de son répertoire et reprises de titres connus de tous.
+                    Retour sur cette soirée d'ouverture ...   
                 </div>
-                    
+
             </div>
             <div class="actu1">
                 <div class="actu1-1">
-                 <iframe class ="frame" width="560" height="315" src="https://www.youtube.com/embed/da1xoQFE7ts" frameborder="0" allowfullscreen></iframe>   
+                    <iframe class ="frame" width="560" height="315" src="https://www.youtube.com/embed/da1xoQFE7ts" frameborder="0" allowfullscreen></iframe>   
                 </div>
                 <div class="actu1-2">
-                 Mariah Carey compte bien faire de son single "Infinity" un tube. Pour mettre toutes les chances de son côté, la chanteuse a convié French Montana,
-                 Justin Bieber et T.I. sur "Why U Mad", un remix rap. Ecoutez ..   
+                    Mariah Carey compte bien faire de son single "Infinity" un tube. Pour mettre toutes les chances de son côté, la chanteuse a convié French Montana,
+                    Justin Bieber et T.I. sur "Why U Mad", un remix rap. Ecoutez ..   
                 </div>
-                    
+
             </div>
-       </div>
-=======
-            <div class="actu1"><div class="actu1-1">http://www.lesinrocks.com/inrocks.tv/aap-rocky-un-clip-retro-pour-jukebox-joints/</div><div class=actu1-2>
-                    Après le clip hallucinogène de L$D, A$AP Rocky est de retour avec Jukebox Joints, morceau issu de son album At.Long.Last
-                    .A$AP sorti en juin dernier. Si les stupéfiants sont toujours au programme, la vidéo commence cette...
-                </div></div>
-            <div class="actu2"></div>
-            <div class="actu3"></div>
-            <div class="actu4"></div>
-            <div class="actu5"></div>
-            <div class="actu6"></div>
         </div>
->>>>>>> origin/master
     </body>
 </html>
